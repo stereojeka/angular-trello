c288de4de8efd3aff4c7e8d73dbe935b0ddeedf6
stereojeka <enimenko@gmail.com>
2017-05-06 00:38:12 +0300

    end tutorial version

---
+++ js/directives/closeEditing.js
@@ -0,0 +1,18 @@
+angular.module('app').directive('closeEditing', function () {
+    var KEYS = {
+        ESCAPE: 27
+    };
+    return {
+        scope: {
+            isEditing: '=' // two-way data binding here
+        },
+        link: function(scope, element, attrs){
+            element.on('keyup', function (e) {
+                if(_.isEqual(e.keyCode, KEYS.ESCAPE)){
+                    scope.isEditing = false;
+                    scope.$apply();
+                }
+            })
+        }
+    }
+})
